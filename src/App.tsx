53cffcdd385d45833cb9770afd662b254690813f
Eric <eric.ledonge@gmail.com>
2022-11-25 15:13:05 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+import React from 'react';
+
+import RefsExercise2 from './components/Refs/RefsExercise2';
+
+function App(): JSX.Element {
+  return <RefsExercise2 />;
+}
+
+export default App;
